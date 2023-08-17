7f341c68256bd0b2c97344909349a7975097e1e2
Kayro França <kayro.dev@gmail.com>
2023-08-17 11:22:17 -0300

    feat(): novo icon menu hamburguer

---
+++ src/app/assets/icon-menu.jsx
@@ -0,0 +1,33 @@
+export default function MenuIcon() {
+	return (
+		<svg
+			width="48"
+			height="48"
+			viewBox="0 0 48 48"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M7.94977 11.9498H39.9498"
+				stroke="currentColor"
+				strokeWidth="4"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M7.94977 23.9498H39.9498"
+				stroke="currentColor"
+				strokeWidth="4"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M7.94977 35.9498H39.9498"
+				stroke="currentColor"
+				strokeWidth="4"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+}
