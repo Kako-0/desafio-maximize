9254f18708ae21859516d94ce5f10f3305b456a5
Kayro França <kayro.dev@gmail.com>
2023-08-17 03:36:59 -0300

    feat(): adicionado footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,18 +3,20 @@ import Header from "./components/header";
 import Forms from "./components/forms";
 import Aside from "./components/aside";
 import Contato from "./components/contato";
+import Footer from "./components/footer";
 
 export default function Home() {
 	return (
 		<>
 			{/* <Header /> */}
 			<div className="container">
-				<main className="container_main">
+				<main className="container__main">
 					<Contato />
 					<Forms />
 				</main>
 				<Aside />
 			</div>
+			<Footer />
 		</>
 	);
 }
