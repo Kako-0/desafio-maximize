b0fc8b00c418362c060fe61d0a576c0f3b18d768
Kayro França <kayro.dev@gmail.com>
2023-08-17 11:25:14 -0300

    feat(): adicionado button para o mobile

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -5,16 +5,21 @@ import TiktokIcon from "@/app/assets/icon-tiktok";
 import TwitterIcon from "@/app/assets/icon-twitter";
 import YoutubeIcon from "@/app/assets/icon-youtube";
 
-import "./styles.css";
+import "./styles.scss";
+import MenuIcon from "@/app/assets/icon-menu";
 
 export default function Header() {
 	return (
 		<header className="header">
-			<a href="./" className="logo">
+			<a href="./" className="header__logo">
 				ARTISTA
 			</a>
 
-			<nav className="nav">
+			<button type="button" className="header__menu">
+				<span className="sr-only">Open main menu</span>
+				<MenuIcon />
+			</button>
+			<nav className="header__nav">
 				<InstagramIcon />
 				<YoutubeIcon />
 				<TwitterIcon />
