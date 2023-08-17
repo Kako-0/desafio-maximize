5fe00cb4a06b29cbba0fa2de189c2043449838d4
Kayro França <kayro.dev@gmail.com>
2023-08-17 11:27:51 -0300

    refactor():  adicionado sass e melhores classes BEM

--- src/app/components/aside/index.jsx
+++ src/app/components/aside/index.jsx
@@ -1,17 +1,17 @@
-import "./styles.css";
+import "./styles.scss";
 
 export default function Aside() {
 	return (
-		<aside>
+		<aside className="aside-menu">
 			<ul className="menu">
-				<li className="menu-item">Sobre</li>
-				<li className="menu-item">Novidades</li>
-				<li className="menu-item">Agenda</li>
-				<li className="menu-item">Galeria</li>
-				<li className="menu-item">Contato</li>
-				<span className="barra" />
-				<li className="menu-item">Imprensa</li>
-				<li className="menu-item">Fã-Clubes</li>
+				<li className="menu__item">Sobre</li>
+				<li className="menu__item">Novidades</li>
+				<li className="menu__item">Agenda</li>
+				<li className="menu__item">Galeria</li>
+				<li className="menu__item">Contato</li>
+				<li className="menu__divider"></li>
+				<li className="menu__item">Imprensa</li>
+				<li className="menu__item">Fã-Clubes</li>
 			</ul>
 		</aside>
 	);
