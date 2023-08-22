a6940f4848e32ef0cf9faead3842c80c784d918f
Kayro França <kayro.dev@gmail.com>
2023-08-22 15:39:19 -0300

    feat(): adicionado comentários importantes

--- src/app/apis/databaseApi.js
+++ src/app/apis/databaseApi.js
@@ -1,14 +1,19 @@
-import { collection, addDoc, getDocs, doc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { database } from "../services/firebase";
 
+// Acessa a coleção no banco de dados
 const mensagensRef = collection(database, "Mensagens");
 
+// Adiciona os dados no banco
 const setDados = async (dados) => {
 	await addDoc(mensagensRef, dados);
 };
 
+// Recupera os dados no banco
 const getDados = async () => {
 	const dados = await getDocs(mensagensRef);
+
+	// Mapeia os documentos retornados pela consulta
 	const mensagens = dados.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
 	return mensagens;
