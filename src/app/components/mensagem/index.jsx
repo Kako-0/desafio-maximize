47ca4894542af0163d4ca907a55e27c1a54c2e05
Kayro França <kayro.dev@gmail.com>
2023-08-22 02:56:04 -0300

    Feat(): adição de props

--- src/app/components/mensagem/index.jsx
+++ src/app/components/mensagem/index.jsx
@@ -1,35 +1,41 @@
 import "./styles.scss";
 
-export default function Mensagem() {
+export default function Mensagem({
+	nome,
+	relacao,
+	nomeEmpresa,
+	email,
+	telefone,
+	mensagem,
+}) {
 	return (
 		<tr className="mensagem">
 			<td scope="row" className="mensagem__conteudo">
 				<details className="mensagem__detalhes">
 					<summary className="mensagem__header">
 						<div>
-							<p className="mensagem__titulo">Fulano</p>
+							<p className="mensagem__titulo">{nome}</p>
 							<span className="mensagem__collapse">Detalhes</span>
 						</div>
-						<p className="mensagem__preview">
-							Olá, mensagem teste Olá, mensagem teste Olá, mensagem teste Olá,
-							mensagem teste Olá, mensagem teste Olá, mensagem teste
-						</p>
+						<p className="mensagem__preview">{mensagem}</p>
 					</summary>
 					<div className="mensagem-details-content">
 						<p>
-							<b>Empresa:</b> Exemplo Ltda.
+							<b>Relacionado à:</b> {relacao}
 						</p>
 						<p>
-							<b>Email:</b> fulano@example.com
+							<b>Empresa:</b> {nomeEmpresa}
 						</p>
 						<p>
-							<b>Telefone:</b> (123) 456-7890
+							<b>Email:</b> {email}
+						</p>
+						<p>
+							<b>Telefone:</b> {telefone}
 						</p>
 						<p>
 							<b>Mensagem:</b>
 							<br />
-							Olá, mensagem teste Olá, mensagem teste Olá, mensagem teste Olá,
-							mensagem teste Olá, mensagem teste Olá, mensagem teste
+							{mensagem}
 						</p>
 					</div>
 				</details>
